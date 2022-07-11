33820e8081958c0c56eabc569c4c66c2e6ea52db
Paweł Łęczycki <pawel@msd.info.pl>
2022-07-11 11:10:03 +0200

    Agregacja

---
+++ OOP/src/Models/Order.java
@@ -0,0 +1,25 @@
+package Models;
+
+import java.security.PublicKey;
+
+public class Order {
+
+    private Product[] products;
+
+    public Order(Product[] products) {
+        this.products = products;
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public float getPrice() {
+        var sum = 0f;
+        for (var product: products) {
+            sum += product.getPrice();
+        }
+        return  sum;
+    }
+
+}
