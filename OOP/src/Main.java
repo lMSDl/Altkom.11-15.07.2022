c26db7544ec3fc0536759be73773ca0dfa615aea
Paweł Łęczycki <pawel@msd.info.pl>
2022-07-11 10:42:29 +0200

    Hermetyzacji

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -4,13 +4,23 @@ public class Main {
     public static void main(String[] args) {
 
         var product1 = new Product();
-        product1.name = "Kubek";
-        product1.price = 32.99f;
+        //product1.name = "Kubek";
+        //product1.price = 32.99f;
+        product1.setName("Kuber");
+        product1.addToPrice(32.99f);
+        System.out.println(product1.getInfo());
+        product1.addToPrice(10);
         System.out.println(product1.getInfo());
 
         var product2 = new Product();
-        product2.name = "Piłka";
-        product2.price = 19.50f;
+        //product2.name = "Piłka";
+        //product2.price = 19.50f;
+        product2.setName("pIŁkA");
+        product2.addToPrice(19.50f);
         System.out.println(product2.getInfo());
     }
+
+    /*static void addToPrice(Product product, float value) {
+        product.price += value;
+    }*/
 }
--- OOP/src/Models/Product.java
+++ OOP/src/Models/Product.java
@@ -2,8 +2,24 @@ package Models;
 
 public class Product {
 
-    public String name;
-    public float price;
+    private String name;
+    private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.toUpperCase();
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void addToPrice(float valueToAdd) {
+        this.price += valueToAdd;
+    }
 
     public String getInfo() {
         return String.format("%s: %.2f zł", name, price);
